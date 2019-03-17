a63305b4844fdd0281d5f77be5686fc75d29f612
yoeswin <yoeswin28@gmail.com>
2019-03-17 15:06:56 +0530

    splash and optimize comments

--- app/src/main/java/com/example/lionertic/main/Fragments/SplashScreen.java
+++ app/src/main/java/com/example/lionertic/main/Fragments/SplashScreen.java
@@ -10,7 +10,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +23,8 @@ import java.util.Objects;
 import static android.content.Context.MODE_PRIVATE;
 
 public class SplashScreen extends Fragment {
+    Handler handler;
+    Runnable runnable;
 
     public SplashScreen() {
         // Required empty public constructor
@@ -41,9 +42,8 @@ public class SplashScreen extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
-
-        new Handler().postDelayed(new Runnable() {
+        handler = new Handler();
+        runnable = new Runnable() {
             @Override
             public void run() {
                 if (isNetworkConnected()) {
@@ -51,7 +51,7 @@ public class SplashScreen extends Fragment {
                     String KEY = preferences.getString("KEY", "");
                     String ID = preferences.getString("ID", "");
                     if (!TextUtils.isEmpty(KEY)) {
-                        new KeyCheck(getContext(),getActivity()).execute(KEY,ID);
+                        new KeyCheck(getContext(), getActivity()).execute(KEY, ID);
                     } else {
                         if (getFragmentManager() != null) {
                             getFragmentManager().beginTransaction().replace(R.id.fragment, new LogIn()).commit();
@@ -60,13 +60,15 @@ public class SplashScreen extends Fragment {
                 } else
                     Toast.makeText(getContext(), "NO network..Enable and RESTART APP", Toast.LENGTH_LONG).show();
             }
-        }, 1500);
+        };
+        handler.postDelayed(runnable, 1500);
     }
 
     private boolean isNetworkConnected() {
         ConnectivityManager cm = (ConnectivityManager) Objects.requireNonNull(getActivity()).getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo() != null;
     }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -76,6 +78,7 @@ public class SplashScreen extends Fragment {
     @Override
     public void onStop() {
         super.onStop();
+        handler.removeCallbacks(runnable);
         ((AppCompatActivity) getActivity()).getSupportActionBar().show();
     }
 
--- app/src/main/java/com/example/lionertic/main/MainActivity.java
+++ app/src/main/java/com/example/lionertic/main/MainActivity.java
@@ -1,30 +1,17 @@
 package com.example.lionertic.main;
 
-import android.Manifest;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Toast;
 
-import com.example.lionertic.main.AsyncTask.KeyCheck;
-import com.example.lionertic.main.Fragments.LogIn;
 import com.example.lionertic.main.Fragments.SplashScreen;
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,121 +36,29 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences preferences = getSharedPreferences("KEY", MODE_PRIVATE);
         KEY = preferences.getString("KEY", "");
-        IID = preferences.getString("ID","");
-//        if (isNetworkConnected()) {
-//            isLocationEnabled();
-//            startLocation(0);
-//            if (KEY.equals("")) {
-                //            startLocation(0);
-                FragmentManager fm = getSupportFragmentManager();
-                fm.beginTransaction().add(R.id.fragment, new SplashScreen()).commit();
-//            } else {
-//                Log.e("asfafsfasaas", "not granted");
-//                startLocation(1);
-//            }
-//        } else
-//            Toast.makeText(getApplicationContext(), "NO network..Enable and RESTART APP", Toast.LENGTH_LONG).show();
+        IID = preferences.getString("ID", "");
+
+
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().add(R.id.fragment, new SplashScreen()).commit();
     }
 
-//    private void openSettings() {
-//        Intent intent = new Intent();
-//        intent.setAction(
-//                Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-//        Uri uri = Uri.fromParts("package",
-//                BuildConfig.APPLICATION_ID, null);
-//        intent.setData(uri);
-//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//        startActivity(intent);
-//    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    public void getdet(){
-     SharedPreferences sd = getSharedPreferences("KEY", MODE_PRIVATE);
-     String keys=sd.getString("KEY","");
-    }
-//    public void startLocation(final int i) {
-//        // Requesting ACCESS_FINE_LOCATION using Dexter library
-//
-//        Dexter.withActivity(activity)
-//                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
-//                .withListener(new PermissionListener() {
-//                    @Override
-//                    public void onPermissionGranted(PermissionGrantedResponse response) {
-//                        if (i == 1) {
-//                            progressDialog.show();
-//                            progressDialog.setMessage("Logging In...");
-//                            progressDialog.setCancelable(false);
-//                            new KeyCheck(context, activity).execute();
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onPermissionDenied(PermissionDeniedResponse response) {
-//                        if (response.isPermanentlyDenied()) {
-//                            // open device settings when the permission is
-//                            // denied permanently
-//
-//                            openSettings();
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onPermissionRationaleShouldBeShown(com.karumi.dexter.listener.PermissionRequest permission, PermissionToken token) {
-//
-//                    }
-//                }).check();
-//    }
 
     @Override
     public void onResume() {
         super.onResume();
-//        if (checkPermissions()) {
-//            mRequestingLocationUpdates = true;
-//        }
-        // Resuming location updates depending on button state and
-        // allowed permissions
 
     }
-
-
-//    private boolean checkPermissions() {
-//        int permissionState = ActivityCompat.checkSelfPermission(this,
-//                Manifest.permission.ACCESS_FINE_LOCATION);
-//        return permissionState == PackageManager.PERMISSION_GRANTED;
-//    }
-
 
     private boolean isNetworkConnected() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo() != null;
     }
 
-//    public void isLocationEnabled() {
-//        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-//        boolean gps_enabled = false, network_enabled = false;
-//        try {
-//            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
-//        } catch (Exception ex) {
-//        }
-//        try {
-//            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-//        } catch (Exception ex) {
-//        }
-//        if (!gps_enabled && !network_enabled) {
-//            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-//            dialog.setMessage("Enable Location");
-//            dialog.setPositiveButton("Enable", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-//                    Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-//                    startActivity(myIntent);
-//                }
-//            });
-//            dialog.show();
-//        }
-//    }
 }
